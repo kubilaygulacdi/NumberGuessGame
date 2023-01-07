ddd0fd60906f84beedc86e00d07e45076c94dbf3
Kubilay Gulacdi <mkgulacdi@gmail.com>
2023-01-07 21:27:00 +0300

    third commit

--- NumberGuessGame.py
+++ NumberGuessGame.py
@@ -6,30 +6,36 @@ def computerGuess(x):
     high = x
     guess = ''
     while guess != "c" and low != high:
+
         randomNumber = random.randint(low,high)
-        guess = input(f"Is it {randomNumber} ?, high(h), low(l), correct(c): ")
+        guess = input(f"Is it {randomNumber}? high(h), low(l), correct(c), quit(q): ")
+
         if guess == "h":
             high = randomNumber - 1
         elif guess == "l":
             low = randomNumber + 1
-            
-        
-    print(f"Guessed number is {randomNumber}")
-        
-
+        elif guess == "c":
+            print(f"the number you chosen is {randomNumber}")
+        elif guess == "q":
+            break
+        else:
+            print("Please choose h,l or c")
+    
+              
 def userGuess(x): #use this function if you want to make a guess yourself
     guess = 0
     random_number = random.randint(1,x)
    
     while guess != random_number:
-        guess = int(input(f"Guess a number between 0 and {x}: "))
-        if guess > random_number:
-            print ("too high")
-        elif guess < random_number:
-            print("too low")
-         
-    print (f"You have guessed the number: {guess}")
-
-
-
-
+        try:
+            guess = int(input(f"Guess a number between 0 and {x}, (-777 to quit): "))
+            if guess == -777:
+                break
+            elif guess > random_number:
+                print ("too high")
+            elif guess < random_number:
+                print("too low")
+            elif guess == random_number:
+                print (f"You have guessed the number: {guess}")
+        except:
+            print("Please guess a number")
